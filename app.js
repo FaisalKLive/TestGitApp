af0e18e7b95a7b09affbbc334204083eeec10f32
FaisalKLive <FaisalKLive@Hotmail.com>
2024-03-24 11:54:13 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+/*
+********* Setting up a Git User Using GitBash ************
+
+Computer@DESKTOP-CC9222K MINGW64 ~
+$ git config --global user.name "FaisalKLive"
+
+Computer@DESKTOP-CC9222K MINGW64 ~
+$ git config --global user.name
+FaisalKLive
+
+Computer@DESKTOP-CC9222K MINGW64 ~
+$ git config --global user.email "FaisalKLive@Hotmail.com"
+
+Computer@DESKTOP-CC9222K MINGW64 ~
+$ git config --global user.email
+FaisalKLive@Hotmail.com
+
+Computer@DESKTOP-CC9222K MINGW64 ~
+
+       **************** =============================== ****************
+$
+
+
+
+*/
+
+console.log("first Git App !!!")
